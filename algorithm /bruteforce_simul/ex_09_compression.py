2b7ec2b8011eeec2e425e900b330fbfac9078291
isakchoe <50509176+isakchoe@users.noreply.github.com>
2021-02-02 17:22:23 +0900

    2/2 pra

--- algorithm /bruteforce_simul/ex_09_compression.py
+++ algorithm /bruteforce_simul/ex_09_compression.py
@@ -1,3 +1,33 @@
-def compression:
-    프로그래머스 참조
-    
+def solution(s):
+    # 프로그래머스 카카오 문자열 압축
+
+
+    # 압축전 값 (길이 1인경우도 포함)
+    answer = len(s)
+
+
+    # 문자열 길이 절반까지만 탐색 (절반을 넘으면 중복이 없기 때문에!)
+
+    for step in range(1, len(s)//2 + 1):
+        prev = s[0:step]
+        count = 1
+
+        compressed = ''
+        for j in range(step, length, step):
+            if prev == s[j: j + step]:
+                count += 1
+
+            else:
+                if count > 1:
+                    compressed += str(count) + prev
+                else:
+                    compressed += prev
+                prev = s[j:j + step]
+                count = 1
+
+        compressed += str(count) + prev if count > 1 else prev
+
+        answer = min(answer, len(compressed))
+
+    return answer
+

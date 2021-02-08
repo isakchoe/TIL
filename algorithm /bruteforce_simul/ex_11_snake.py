664392b5af0a8bc22bb869bc4b508e187d7e0610
isakchoe <50509176+isakchoe@users.noreply.github.com>
2021-02-08 22:39:07 +0900

    2/8 pra

--- algorithm /bruteforce_simul/ex_11_snake.py
+++ algorithm /bruteforce_simul/ex_11_snake.py
@@ -1,3 +1,5 @@
+
+
 def main():
 
     n = int(input())
@@ -6,8 +8,8 @@ def main():
     apple_loca = []
 
     for i in range(num_apple):
-        x,y = map(int,input().split())
-        apple_loca.append([x-1,y-1])
+        x, y = map(int, input().split())
+        apple_loca.append([x,y])
 
     num_turn = int(input())
 
@@ -18,71 +20,67 @@ def main():
         turn_list.append([int(time),look])
 
 
-    head_row = 0
-    head_col = 0
+    head_row = 1
+    head_col = 1
 
-    path_list = [[head_row,head_col]]
+    path_list = [(head_row,head_col)]
+
 
     dir = 'R'
 
-    map = [[0]*n for _ in range(n)]
+    data = [[0]*(n+2) for _ in range(n+2)]
 
 
     # 사과 놓인 곳을 1로 만듬.
     for i in range(num_apple):
         x, y = apple_loca[i][0], apple_loca[i][1]
-        map[x][y] = 1
+        data[x][y] = 1
+
+    data[head_row][head_col] = 2
+
 
     time = 0
+
+    move = [[0,1], [1,0], [0,-1], [-1,0]]
+
+
+    direction = 0
 
     while True:
 
         head_row , head_col = path_list[-1][0], path_list[-1][1]
+
+        pro_row, pro_col = head_row +move[direction][0], head_col+move[direction][1]
+
+
+        # 끝나지 않는경우
+        if pro_row >0 and pro_row  <=n and pro_col >0 and pro_col <=n and data[pro_row][pro_col] !=2:
+
+            if data[pro_row][pro_col] !=1:
+                rx,ry = path_list.pop(0)
+                data[rx][ry] = 0
+
+            path_list.append([pro_row, pro_col])
+            data[pro_row][pro_col] = 2
+
+            time +=1
+
+        # 끝나는 경우
+        else:
+            time +=1
+            break
 
         for i in range(len(turn_list)):
             if time == turn_list[i][0]:
                 dir = turn_list[i][1]
 
 
-
-
-        if dir == "R":
-            if map[path_list[-1][0]][path_list[-1][1]+1] != 1:
-                path_list.pop(0)
-
-            path_list.append([path_list[-1][0], path_list[-1][1] + 1])
-
-        if dir == "L":
-            if map[head_row][head_col-1] != 1:
-                path_list.pop(0)
-
-            path_list.append([head_row, head_col-1])
-
-        if dir == "U":
-            if map[head_row-1][head_col] !=1:
-                path_list.pop(0)
-
-            path_list.append([head_row-1, head_col])
-
-        if dir == "D":
-            if map[head_row+1, head_col] !=1:
-                path_list.pop(0)
-            path_list.append([head_row+1, head_col])
-
-
-        time +=1
-
-
-        if  path_list[-1][0] > n or path_list[-1][0] < 0 or path_list[-1][1] > n or path_list[-1][1] < 0 :
-            break
-
-        # 몸이 부딪히는 경우
-        for i in range(len(path_list)-1):
-            for j in range(i+1, len(path_list)):
-                if path_list[i] == path_list[j] :
-                    break
-
+                if dir == "L":
+                    direction = (direction - 1) % 4
+                else:
+                    direction = (direction + 1) % 4
 
 
     print(time)
-    
+
+main()

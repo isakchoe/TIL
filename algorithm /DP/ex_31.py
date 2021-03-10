b8c52b2305ad37b37e7ec1faa990b79d74c81849
isakchoe <50509176+isakchoe@users.noreply.github.com>
2021-03-10 23:53:23 +0900

    3/10 pra

---
+++ algorithm /DP/ex_31.py
@@ -0,0 +1,45 @@
+
+t = int(input())
+
+for tc in range(t):
+    n, m = map(int, input().split())
+
+    dp = []
+
+    data = list(map(int, input().split()))
+
+
+    index = 0
+
+
+    # 초기 테이블 세팅
+    for i in range(n):
+        dp.append(data[index:index+m])
+        index += m
+
+
+    for col in range(1,m):
+
+        for row in range(n):
+
+            if row -1 >=0:
+                up = dp[row-1][col-1]
+            else:
+                up = dp[row][col-1]
+
+            if row +1 <n:
+                down = dp[row+1][col-1]
+            else:
+                down = dp[row][col-1]
+
+            dp[row][col] += max(up,down, dp[row][col-1])
+
+
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m-1])
+
+    print(result)
+
+
+

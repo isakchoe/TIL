e5e6c3d1654c117cc7e242d6cdd070364b6b1ec5
isakchoe <50509176+isakchoe@users.noreply.github.com>
2021-05-05 16:10:55 +0900

    5/4 and 5/5 pra

---
+++ algorithm /order_by/26Retry.py
@@ -0,0 +1,18 @@
+#
+# # 입력받기
+# n = int(input())
+# num = []
+#
+# for i in range(n):
+#     num.append(int(input()))
+#
+# num.sort()
+#
+# total = (n-1)*num[0]
+#
+# for i in range(1,n):
+#     total += (n-i)*num[i]
+#
+# print(total)
+
+# 틀렸다!. 힙 -- 개념 공부

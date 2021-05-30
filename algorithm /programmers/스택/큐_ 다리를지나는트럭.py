3c2dd78052e15dbc2c1bcd253d42d88de97c807e
isakchoe <50509176+isakchoe@users.noreply.github.com>
2021-05-30 22:17:26 +0900

    5/30 pra

---
+++ algorithm /programmers/스택/큐_ 다리를지나는트럭.py
@@ -0,0 +1,55 @@
+
+# 잘못된 풀이, 로직
+
+
+#
+# from _collections import deque
+#
+#
+# def solution(bridge_length, weight, truck_weights):
+#
+#     q = deque()
+#
+#
+#    #  큐 == 다리, 0으로 채워넣기
+#     for _ in range(bridge_length):
+#         q.append(0)
+#
+#
+#     index = 0
+#     time = 0
+#     total = 0
+#
+#    #  초당 pop
+#
+#     while True:
+#
+#
+#         # 다리 무게가 견딜 수 있으면, 트럭 진입 push
+#         if total + truck_weights[index] <= weight:
+#             q.append(truck_weights[index])
+#             total += truck_weights[index]
+#             total -= q.popleft()
+#
+#             index += 1
+#
+#         # 진입못하면
+#         else:
+#             q.append(0)
+#             total -= q.popleft()
+#
+#             if total == 0:
+#                 q.append(truck_weights[index])
+#                 total += truck_weights[index]
+#                 total -= q.popleft()
+#                 index += 1
+#
+#         time += 1
+#
+#         if index == len(truck_weights) :
+#             break
+#
+#
+#     return time + bridge_length
+#
+#

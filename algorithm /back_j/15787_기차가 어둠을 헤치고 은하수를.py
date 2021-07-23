1c174386e3280c05693458ea446b85520ce45dc9
최부성 <isakchoe@naver.com>
2021-07-23 21:18:18 +0900

    7/23 pra

--- algorithm /back_j/15787_기차가 어둠을 헤치고 은하수를.py
+++ algorithm /back_j/15787_기차가 어둠을 헤치고 은하수를.py
@@ -3,24 +3,29 @@
 # 입력받고, 명령 수행
 # 계산
 
+import sys
+input = sys.stdin.readline
+
 
 def sit_down(i,x):
+    # x 는 -1 해서 받기
     train_list[i][x-1] = 1
 
 def stand_up(i,x):
     train_list[i][x-1] = 0
 
 def move_back(i):
-
     for index in range(19,0,-1):
         train_list[i][index] = train_list[i][index-1]
+    # 맨앞 처리
     train_list[i][0] = 0
 
 def move_forward(i):
     for index in range(19):
         train_list[i][index] = train_list[i][index+1]
-
+    # 맨뒤 처리
     train_list[i][19] = 0
+
 
 # 입력
 n,m = map(int, input().split())
@@ -30,36 +35,41 @@ train_list = [[0]*20 for _ in range(n+1)]
 
 # 명령수행
 for _ in range(m):
-    command = input()
+    command = tuple(map(int, input().split()))
 
-    i = int(command[2])
-
-    if command[0] == '1':
-        x = int(command[4])
+    if command[0] == 1:
+        i = command[1]
+        x = command[2]
         sit_down(i,x)
-    elif command[0] == '2':
-        x = int(command[4])
+    elif command[0] == 2:
+        i = command[1]
+        x = command[2]
         stand_up(i,x)
-    elif command[0] == '3':
+    elif command[0] == 3:
+        i = command[1]
         move_back(i)
     else:
+        i = command[1]
         move_forward(i)
 
 
-print(train_list)
-
-
 # 탐색
+train_dic = {}
 total = 0
 
-train_dic = {}
-
-for train in train_list:
-    temp = map(str, train)
+for i in range(1,n+1):
+    temp = map(str, train_list[i])
     temp_key = ''.join(temp)
 
     if temp_key not in train_dic:
         train_dic[temp_key] = 1
         total += 1
-
 print(total)
+
+in
+
+# 첫번째 실수,,, 인덱스 20,19,,,, 개수랑 번호수 조율하는거 중요
+# 두번재실수, 입력값 split, 자리수로 해서 index, value error
+# 세번쩨,  0번째 기차는 포함되지 않아야 하는데 포함시켰다.
+
+# 세번째 ==> import sys 로 해결

29303d803537c53f070c6d396a053e6526fc0ecb
최부성 <isakchoe@naver.com>
2021-08-13 21:47:43 +0900

    8/13 pra

--- algorithm /back_j/15723_n단논법.py
+++ algorithm /back_j/15723_n단논법.py
@@ -16,8 +16,10 @@ INF = int(1e9)
 data = [[INF]*(26) for _ in range(26)]
 
 # 알파벳 매핑위한 딕셔너리
-dic = {'a':0, 'b': 1, "c": 2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, 'k':10, "l":11, "m":12, "n":13, "o":14, "p":15,
-       "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25}
+# dic = {'a':0, 'b': 1, "c": 2, "d":3, "e":4, "f":5, "g":6, "h":7, "i":8, "j":9, 'k':10, "l":11, "m":12, "n":13, "o":14, "p":15,
+#        "q":16, "r":17, "s":18, "t":19, "u":20, "v":21, "w":22, "x":23, "y":24, "z":25}
+
+dic = {k:v for v,k in enumerate("abcdefghijklmnopqrstuvwxyz")}
 
 for _ in range(n):
     first, iss, last = input().split()

fab9c507accbff6cbd2e314018e4390a39352356
최부성 <isakchoe@naver.com>
2021-10-18 22:08:28 +0900

    10/18 pra

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 
+
+
 //#include <bits/stdc++.h>
 #include <iostream>
 #include <vector>
@@ -7,6 +9,7 @@
 
 using namespace std;
 
+//1795 번
 
 //로직
 // 다익스트라
@@ -17,6 +20,9 @@ using namespace std;
 
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
 
     int t;
     cin >> t;
@@ -26,7 +32,8 @@ int main(){
         cin >> n >> m >> x;
 
         // 간선 연결 배열 -- 벡터로 표
-        vector<vector<pair<int,int>>> ad_list(n+1) ;
+        vector<vector<pair<int,int>>> ad_list(n+1);
+        vector<vector<pair<int,int>>> reverse_ad_list (n+1);
 
         for(int j=0;j<m;j++){
             int a,b,cost;
@@ -34,12 +41,15 @@ int main(){
 
             // 입력받아서 넣기, a 노드에서 b 노드까지 cost 비용 든다.
             ad_list[a].push_back(make_pair(cost,b));
+            reverse_ad_list[b].push_back(make_pair(cost,a));
         }
 
         vector<int> distance(n+1,100*m+1);
         priority_queue<pair<int,int>> pq;
         // cost, node
         pq.push(make_pair(0,x));
+        // 본인 출발 0
+        distance[x] = 0;
 
 
         // x 노드에서 --> 다른 노드로 최단 경로 테이블 구하기
@@ -51,9 +61,9 @@ int main(){
 
             if(distance[now] < cost) continue;
 
-            for(int i=0; i<ad_list[now].size(); i++){
-                int length = ad_list[now][i].first;
-                int node = ad_list[now][i].second;
+            for(int k=0; k < ad_list[now].size(); k++){
+                int length = ad_list[now][k].first;
+                int node = ad_list[now][k].second;
 
                 int new_cost = cost + length;
 
@@ -65,52 +75,58 @@ int main(){
             }
         }
 
-        //  갔다가 왔다 경로 합치는 테이블
-        vector<int> result_table(n+1,0);
+            // 간선리스트 역방향   특정노드 --> x 노드까지 거리
+        vector<int> reverse_distance(n+1,100*m+1);
+        priority_queue<pair<int,int>> reverse_pq;
+        // cost, node
+        reverse_pq.push(make_pair(0,x));
+        // 본인 출발 0
+        reverse_distance[x] = 0;
 
-        //   i 노드에서 --> x 까지 최단 경로 구하기
-        for(int i=1; i< n+1; i++){
-            priority_queue<pair<int,int>> temp_pq;
-            vector<int> temp_distance(n+1, 100*m+1);
 
-            temp_pq.push(make_pair(0,i));
+        // x 노드에서 --> 다른 노드로 최단 경로 테이블 구하기
+        while(!reverse_pq.empty()){
+            // 꺼내고 마이너스 제거
+            int cost = -reverse_pq.top().first;
+            int now = reverse_pq.top().second;
+            reverse_pq.pop();
 
-            while(!temp_pq.empty()){
-                // 꺼내고 마이너스 제거
-                int cost = -temp_pq.top().first;
-                int now = temp_pq.top().second;
-                temp_pq.pop();
+            if(reverse_distance[now] < cost) continue;
 
-                if(distance[now] < cost) continue;
+            for(int k=0; k < reverse_ad_list[now].size(); k++){
+                int length = reverse_ad_list[now][k].first;
+                int node = reverse_ad_list[now][k].second;
 
-                for(int i=0; i<ad_list[now].size(); i++){
-                    int length = ad_list[now][i].first;
-                    int node = ad_list[now][i].second;
+                int new_cost = cost + length;
 
-                    int new_cost = cost + length;
-
-                    if(temp_distance[node] > new_cost){
-                        temp_distance[node] = new_cost;
-                        // max heap
-                        temp_pq.push(make_pair(-new_cost,node ));
-                    }
+                if(reverse_distance[node] > new_cost){
+                    reverse_distance[node] = new_cost;
+                    // max heap
+                    reverse_pq.push(make_pair(-new_cost,node ));
                 }
             }
-            // result 테이블 갱신
-            result_table[i] = temp_distance[x] + distance[i];
         }
+
+        vector<int>result_table (n+1);
+
+        for(int index =1; index<n+1;index++){
+            result_table[index] = distance[index] + reverse_distance[index];
+        }
+
+
 
         int answer;
-        // 최대값 구하기
-        answer = *max_element(result_table.begin(), result_table.end());
+            // 최대값 구하기
+            answer = *max_element(result_table.begin(), result_table.end());
 
-        for(int i=0; i<result_table.size();i++){
-            cout << result_table[i] <<" ";
+
+//            for (int i = 0; i < result_table.size(); i++) {
+//                cout << result_table[i] << " ";
+//            }
+//            cout << '\n';
+
+            cout << "#" << i << " " << answer << "\n";
+
         }
-        cout <<endl;
-
-        cout << "#" << i << " " << answer <<endl;
-    }
-
     return 0;
 }
